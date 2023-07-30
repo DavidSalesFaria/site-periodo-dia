65823cc387b03e3cddc1e268e4e9233705051594
David Shelton <david.faria012@gmail.com>
2023-07-29 21:19:38 -0300

    Alterando script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+function temaDia(){
+    img.src = "imagens/foto-manha.png"
+    periodo = "AM"
+    window.document.body.style.background = "#feeacd"
+}
+function temaTarde(){
+    img.src = "imagens/foto-tarde.png"
+    window.document.body.style.background = "#fea868"
+}
+function temaNoite(){
+    img.src = "imagens/foto-noite.png"
+    window.document.body.style.background = "#7e5596"
+}
+
 function carregar(){
+    var selecionar = window.document.getElementById("selecionar")
+    var val_selc = ""
+
     // Ober os elementos msg e imagem pelo id
     var msg = window.document.getElementById("msg")
     var img = window.document.getElementById("imagem")
@@ -9,24 +26,24 @@ function carregar(){
     // Período do dia (AM/PM) inicializado com PM
     periodo = "PM"
 
-
     if (horas >= 0 && horas < 12){
         // Bom dia
-        img.src = "imagens/foto-manha.png"
-        periodo = "AM"
-        window.document.body.style.background = "#feeacd"
+        temaDia()
     }
     else if (horas >= 12 && horas < 18){
         // Boa tarde
-        img.src = "imagens/foto-tarde.png"
-        window.document.body.style.background = "#fea868"
+       temaTarde()
     }
     else{
         // Boa noite
-        img.src = "imagens/foto-noite.png"
-        window.document.body.style.background = "#7e5596"
+        temaNoite()
     }
 
     // Altera a mensagem para exibir as horas
     msg.innerHTML = `As horas são ${horas}:${minutos} ${periodo}.`
+
+
+    selecionar.addEventListener("change", function(){
+        val_selc = selecionar.value
+    })
 }
